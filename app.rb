3ff9b546d3b8c30b6221fcd294502e6d352998eb
github username <patcarrasco@outlook.com>
2018-12-09 20:10:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  attr_accessor :name, :hometown, :fav_song
+
+  name= "Pat Carrasco"
+  hometown= "Merrick, NY"
+  fav_song= "Blood on the Leaves"
+
+  get('/name'){
+    "My name is #{name}"
+  }
+
+  get('/hometown') {
+    "My hometown is #{hometown}"
+  }
+
+  get '/favorite-song' do
+    "My favorite song is #{fav_song}"
+  end
+
 end
